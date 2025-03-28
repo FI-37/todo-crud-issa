3b69c6b6b869e929c1200aa3d020286acd1e8f86
Tissa <issathaer7@gmail.com>
2025-03-28 08:45:30 +0000

    Add initial TODO list implementation with API and logging

---
+++ todo.api.php
@@ -0,0 +1,35 @@
+<?php
+header('Content-Type: application/json');
+
+// LOG function in PHP
+
+function write_log($action, $data) {
+
+    $log = fopen('log.txt', 'a');
+
+    $timestamp = date('Y-m-d H:i:s');
+
+    fwrite($log, "$timestamp - $action: " . json_encode($data) . "\n");
+
+    fclose($log);
+
+}
+
+write_log( "init",["empty"] );
+ 
+ 
+switch ($_SERVER['REQUEST_METHOD']) {
+    case 'GET':
+        // Placeholder for reading TODO items
+        break;
+    case 'POST':
+        // Placeholder for creating a new TODO
+        break;
+    case 'PUT':
+        // Placeholder for updating a TODO
+        break;
+    case 'DELETE':
+        // Placeholder for deleting a TODO
+        break;
+}
+?>
