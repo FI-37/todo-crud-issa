bc89104ec09154f567f36e5f7b3c14977605c7ae
Tissa <issathaer7@gmail.com>
2025-03-29 11:03:38 +0000

    add the delete function on php

--- todo.api.php
+++ todo.api.php
@@ -2,39 +2,61 @@
 header('Content-Type: application/json');
 
 // LOG function in PHP
-
 function write_log($action, $data) {
-
     $log = fopen('log.txt', 'a');
-
     $timestamp = date('Y-m-d H:i:s');
-
     fwrite($log, "$timestamp - $action: " . json_encode($data) . "\n");
-
     fclose($log);
-
 }
 
-$todo_items = [
-    ["id" => "someUniqueId", "title" => "Erste Aufgabe"]
-];
+// Read content of the file and decode JSON data to an array.
+$todo_file = 'todo.json';
+if (file_exists($todo_file)) {
+    $todo_items = json_decode(
+        file_get_contents($todo_file),
+        true);
+} else {
+    $todos_items = [];
+}
 
-
- 
  
 switch ($_SERVER['REQUEST_METHOD']) {
     case 'GET':
+        // Placeholder for reading TODO items
         echo json_encode($todo_items);
         write_log("GET", $todo_items);
         break;
     case 'POST':
-        // Placeholder for creating a new TODO
+        // Get data from the input stream.
+        $data = json_decode(file_get_contents('php://input'), true);
+        // Create new todo item.
+        $new_todo = ["id" => uniqid(), "title" => $data['title']];
+        // Add new item to our todo item list.
+        $todo_items[] = $new_todo;
+        // Write todo items to JSON file.
+        file_put_contents($todo_file, json_encode($todo_items));
+        // Return the new item.
+        echo json_encode($new_todo);
         break;
     case 'PUT':
         // Placeholder for updating a TODO
         break;
     case 'DELETE':
-        // Placeholder for deleting a TODO
+        // Get data from the input stream.
+        $data = json_decode(file_get_contents('php://input'), true);
+        // Filter Todo to delete from the list.
+        $todo_items = array_values(
+            array_filter($todo_items,
+                function($todo) use ($data) {
+                    return $todo['id'] !== $data['id'];
+        }));
+        // Write the Todos back to JSON file.
+        file_put_contents('todo.json', json_encode($todo_items));
+        // Tell the client the success of the operation.
+        echo json_encode(['status' => 'success']);
+        write_log("DELETE", $data);
         break;
 }
+
+
 ?>
