9393bb3f58d2ddd636b572b8fca7da4b1fe70292
Tissa <issathaer7@gmail.com>
2025-04-03 08:30:40 +0000

    Add config file for sensitive data.

---
+++ config.example.php
@@ -0,0 +1,11 @@
+<?php
+// Please add the appropriate config settings
+// and rename this file to config.php.
+
+$host = '127.0.0.1';
+$db = 'todo_list';
+$user = 'lu';
+$pass = '12345';
+$charset = 'utf8mb4';
+
+?>
